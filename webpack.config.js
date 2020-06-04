c7473495deaa9dcdf19b52e5674a2c1faaddd14c
Monkey Do <contact@monkeydo.digital>
2020-06-04 19:36:08 +0200

    Bring typescript into the mix

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './static/react-app.jsx',
+  entry: './static/react-app.tsx',
   output: {
 	filename: 'react-app.js',
 	path: path.resolve(__dirname, 'static')
@@ -10,7 +10,7 @@ module.exports = {
   module: {
 	rules: [
 		{
-		test: /\.jsx$/,
+		test: /\.tsx$/,
 		exclude: /node_modules/,
 		use: {
 			loader: 'babel-loader'
